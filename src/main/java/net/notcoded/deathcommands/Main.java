1dbaeeb42a20740a4bc9142b84c1594cbc1bc806
NotCoded <66999075+not-coded@users.noreply.github.com>
2023-03-05 11:58:11 +0100

    small changes

--- src/main/java/net/notcoded/deathcommands/Main.java
+++ src/main/java/net/notcoded/deathcommands/Main.java
@@ -3,9 +3,7 @@ package net.notcoded.deathcommands;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
@@ -47,17 +45,10 @@ public class Main implements ModInitializer {
 				GLFW.GLFW_KEY_Y, // The keycode of the key
 				"deathcommands.title" // The translation key of the keybinding's category.
 		));
-
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while (keyBinding.wasPressed()) {
-				client.openScreen(AutoConfig.getConfigScreen(ClientModConfig.class, null).get());
-			}
-		});
 	}
 
 	private void loadServer() {
 		AutoConfig.register(ServerModConfig.class, GsonConfigSerializer::new);
 		serverModConfig = AutoConfig.getConfigHolder(ServerModConfig.class).getConfig();
-		ServerLifecycleEvents.SERVER_STARTED.register((minecraftServer) -> server = minecraftServer);
 	}
 }
---
+++ src/main/java/net/notcoded/deathcommands/mixin/client/MinecraftClientMixin.java
@@ -0,0 +1,23 @@
+package net.notcoded.deathcommands.mixin.client;
+
+import me.shedaniel.autoconfig.AutoConfig;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.notcoded.deathcommands.Main;
+import net.notcoded.deathcommands.config.ClientModConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Environment(EnvType.CLIENT)
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+    @Inject(at = @At("HEAD"), method = "tick")
+    private void tick(CallbackInfo ci){
+        while (Main.keyBinding.wasPressed()) {
+            Main.client.openScreen(AutoConfig.getConfigScreen(ClientModConfig.class, null).get());
+        }
+    }
+}
